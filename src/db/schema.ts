bcac88c64311dd9e3688850cec8e1e71bbc1a875
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-06-26 17:56:20 -0300

    feat: add server actions drizzle init project

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { boolean, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const todos = pgTable("todos", {
+  id: serial("id").primaryKey(),
+  completed: boolean("completed").default(false),
+  text: text("text").notNull(),
+});
+
+export type Todo = typeof todos.$inferSelect;
